622096a6890fe918e5d5e78ac6d6e656e8d543a2
Ghulam Rasool <ghulamrasoolcs36@gmail.com>
2025-10-08 12:26:17 +0500

    fff

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,22 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import path from "path";
+// import { componentTagger } from "lovable-tagger";
+
+// // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// }));
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
@@ -5,6 +24,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ghulam-rasool/", // ✅ this enables GitHub Pages deployment
   server: {
     host: "::",
     port: 8080,
@@ -16,3 +36,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
